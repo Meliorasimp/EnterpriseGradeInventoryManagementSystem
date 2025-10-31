8329306036b72978d0501ed15e3ae7e5aeade435
Meinard Legashki Limlengco <meliorasimp@gmail.com>
2025-11-01 01:13:26 +0800

    Initial Commit?

--- EnterpriseGradeInventoryAPI/Program.cs
+++ EnterpriseGradeInventoryAPI/Program.cs
@@ -1,7 +1,6 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -16,9 +15,11 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod()
     );
 });
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
+
 
 
 // Configure the HTTP request pipeline.
@@ -32,28 +33,4 @@ app.UseCors("AllowClient");
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- EnterpriseGradeInventoryClient/src/pages/Landingpage.tsx
+++ EnterpriseGradeInventoryClient/src/pages/Landingpage.tsx
@@ -1,7 +1,10 @@
+import "../styles/index.css";
+import Landingpagenavbar from "../components/Landingpagenavbar";
 const Landingpage = () => {
   return (
-    <div>
-      <h1>Welcome to the Landing Page</h1>
+    <div className="min-h-screen">
+      <Landingpagenavbar />
+      <h1 className="text-4xl font-bold"></h1>
     </div>
   );
 };
