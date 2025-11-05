51524d1ec22bdda964d04c555ac4f2d1e49b5f04
Meinard Legashki Limlengco <meliorasimp@gmail.com>
2025-11-05 15:34:27 +0800

    Changed the Name of the batchfile and edited some scripts

--- startproject.bat
+++ startproject.bat
@@ -1,7 +1,5 @@
 @echo off
-echo ====================================
 echo Starting Enterprise Grade Inventory System
-echo ====================================
 
 REM Start the backend (.NET API) in a new terminal window
 start "Backend" cmd /k "cd /d C:\Users\melio\OneDrive\Documents\Website Projects\EnterpriseGradeInventoryManagementSystem\EnterpriseGradeInventoryAPI && dotnet run --launch-profile https"
@@ -9,7 +7,6 @@ start "Backend" cmd /k "cd /d C:\Users\melio\OneDrive\Documents\Website Projects
 REM Start the frontend (React/Vite app) in another terminal window
 start "Frontend" cmd /k "cd /d C:\Users\melio\OneDrive\Documents\Website Projects\EnterpriseGradeInventoryManagementSystem\EnterpriseGradeInventoryClient && npm run dev"
 
-echo ====================================
+
 echo Backend and Frontend are starting...
-echo ====================================
 pause
